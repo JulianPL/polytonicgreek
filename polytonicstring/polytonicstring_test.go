8b7e8633462b1ccc649630031c4d156735021825
JulianPL <j.pape-lange@gmx.de>
2017-09-09 01:31:08 +0200

    Add testcases for polytonicstring

--- polytonicstring/polytonicstring_test.go
+++ polytonicstring/polytonicstring_test.go
@@ -1,31 +1,9 @@
 package polytonicstring
 
 import (
-	"polytonicgreek/polytonicchar"
+	//"polytonicgreek/polytonicchar"
 	"testing"
 )
-
-func TestNew(t *testing.T) {
-	cases := []struct {
-		in   string
-		want string
-	}{
-		{
-			"Au)ta\\r o( e)k lime/nos",
-			"\u0391\u1f50\u03c4\u1f70\u03c1 \u1f41 \u1f10\u03ba \u03bb\u03b9\u03bc\u1f73\u03bd\u03bf\u03c2",
-		},
-		{
-			"o(",
-			"\u1f41",
-		},
-	}
-	for _, c := range cases {
-		got := New(c.in).String()
-		if got != c.want {
-			t.Errorf("New(%q).String() == %q, want %q", c.in, got, c.want)
-		}
-	}
-}
 
 func TestBetaToStr(t *testing.T) {
 	cases := []struct {
@@ -33,8 +11,20 @@ func TestBetaToStr(t *testing.T) {
 		want string
 	}{
 		{
-			"Au)ta\\r o( e)k lime/nos",
-			"\u0391\u1f50\u03c4\u1f70\u03c1 \u1f41 \u1f10\u03ba \u03bb\u03b9\u03bc\u1f73\u03bd\u03bf\u03c2",
+			"",
+			"",
+		},
+		{
+			"W)h|u=iA)eo(",
+			"\u1f68\u1fc3\u1fe6\u03b9\u1f08\u03b5\u1f41",
+		},
+        {
+			". :\n,';",
+			". \u0387\n,';",
+		},
+        {
+			"sys",
+			"\u03c3\u03c8\u03c2",
 		},
 	}
 	for _, c := range cases {
@@ -45,28 +35,39 @@ func TestBetaToStr(t *testing.T) {
 	}
 }
 
+func TestNew(t *testing.T) {
+	cases := []struct {
+		in   string
+	}{
+		{
+			"Au)ta\\r o( e)k lime/nos",
+		},
+	}
+	for _, c := range cases {
+		got := New(c.in).String()
+        want := BetaToStr(c.in).String()
+		if got != want {
+			t.Errorf("New(%q).String() == %q, want %q", c.in, got, want)
+		}
+	}
+}
+
 func TestInString(t *testing.T) {
 	cases := []struct {
-		inChar *polytonicchar.PolytonicChar
+		inChar string
 		inStr  string
 		exact  bool
 		want   int
 	}{
-		{
-			polytonicchar.New(polytonicchar.Tau, true),
-			"Au)ta\\r o( e)k lime/nos",
-			false,
-			2,
-		},
-		{
-			polytonicchar.New(polytonicchar.Tau, true),
-			"Au)ta\\r o( e)k lime/nos",
-			true,
-			-1,
-		},
+		{"I=", "Au)ta\\r o( e)k lime/nos", false, 12,},
+        {"g", "Au)ta\\r o( e)k lime/nos", false, -1,},
+        {"A", "Au)ta\\r o( e)k lime/nos", true, 0,},
+        {"e)", "Au)ta\\r o( e)k lime/nos", true,8,},
+        {"A(", "Au)ta\\r o( e)k lime/nos", true, -1,},
+		{"a", "Au)ta\\r o( e)k lime/nos", true, -1,},
 	}
 	for _, c := range cases {
-		got := InString(c.inChar, New(c.inStr), c.exact)
+		got := InString(New(c.inChar)[0], New(c.inStr), c.exact)
 		if got != c.want {
 			t.Errorf("InString(%q, New(%q), %t) == %d, want %d", c.inChar, c.inStr, c.exact, got, c.want)
 		}
@@ -80,29 +81,50 @@ func TestSubString(t *testing.T) {
 		exact bool
 		want  int
 	}{
-		{
-			"o( e(",
-			"Au)ta\\r o( e)k lime/nos",
-			false,
-			6,
-		},
-		{
-			"o( e(",
-			"Au)ta\\r o( e)k lime/nos",
-			true,
-			-1,
-		},
-		{
-			"o( e)",
-			"Au)ta\\r o( e)k lime/nos",
-			true,
-			6,
-		},
+		{"o( e(", "Au)ta\\r o( e)k lime/nos", false, 6,},
+        {"o( e)", "Au)ta\\r o( e)k lime/nos", false, 6,},
+		{"o( e(", "Au)ta\\r o( e)k lime/nos", true,-1,},
+		{"o( e)", "Au)ta\\r o( e)k lime/nos", true, 6,},
 	}
 	for _, c := range cases {
 		got := SubString(New(c.inSub), New(c.inStr), c.exact)
 		if got != c.want {
 			t.Errorf("InString(New(%q), New(%q), %t) == %d, want %d", c.inSub, c.inStr, c.exact, got, c.want)
+		}
+	}
+}
+
+func TestWrapString(t *testing.T) {
+    cases := []struct {
+		in    string
+        width int
+		want  string
+	}{
+		{
+            "oo o o o o o o o",
+            5,
+            "\u03bf\u03bf \u03bf\n\u03bf \u03bf \u03bf\n\u03bf \u03bf \u03bf",
+        },
+		{
+            "oo o o o o o o o",
+            4,
+            "\u03bf\u03bf \u03bf\n\u03bf \u03bf\n\u03bf \u03bf\n\u03bf \u03bf",
+        },
+        {
+            "oo\no o o o o o o",
+            5,
+            "\u03bf\u03bf\n\u03bf \u03bf \u03bf\n\u03bf \u03bf \u03bf\n\u03bf",
+        },
+        {
+            "oooooo oooo ooo oo o",
+            4,
+            "\u03bf\u03bf\u03bf\u03bf\u03bf\u03bf\n\u03bf\u03bf\u03bf\u03bf\n\u03bf\u03bf\u03bf\n\u03bf\u03bf \u03bf",
+        },
+    }
+	for _, c := range cases {
+        got := WrapString(New(c.in), c.width).String()
+		if got != c.want {
+			t.Errorf("WrapString(New(%q), %v).String() == %q, want %q", c.in, c.width, got, c.want)
 		}
 	}
 }
